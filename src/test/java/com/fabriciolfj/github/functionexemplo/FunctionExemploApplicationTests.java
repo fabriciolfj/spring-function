5c1d1050642ad8d4677c653b74a19f41c4c72229
fabricio <fabricio.jacob@outlook.com>
2020-10-21 20:38:18 -0300

    init project.

---
+++ src/test/java/com/fabriciolfj/github/functionexemplo/FunctionExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciolfj.github.functionexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
